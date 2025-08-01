c44930ea8b42814f38cd7232534ed5a4e68d5540
QuantumHund <balazsgazso.hu@gmail.com>
2025-08-02 00:15:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import streamlit as st
 import yfinance as yf
 import pandas as pd
-from ta.momentum import RSIIndicator
 
-st.title("TA teszt: RSI SPX")
+st.title("SPX teszt: Close árak")
 
 spx = yf.download("^GSPC", period="3mo", interval="1d")
 if not spx.empty:
-    spx['RSI'] = RSIIndicator(close=spx['Close'], window=14).rsi()
-    st.line_chart(spx[['Close', 'RSI']])
+    st.line_chart(spx['Close'])
 else:
     st.error("❌ Nem sikerült SPX adatot letölteni.")
